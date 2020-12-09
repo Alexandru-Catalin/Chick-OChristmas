99ad563579200bf1b34ed9441e7561cc255367fa
Corvus Corax <alexandrumarcu479@gmail.com>
2020-12-09 21:19:46 +0000

    Camera Follow The Player + Character Running on Z

---
+++ Mobile Endless Game/Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+        //Camera will follow the Target at Z position - 10 and freezes positions X and Y
+        Vector3 newPos = new Vector3(0, target.transform.position.y + 3, target.transform.position.z - 10);
+        transform.position = newPos;
+
+        //Freez camera Rotations on X, Y, Z
+        Quaternion newPos2 = Quaternion.Euler(transform.rotation.x + 10, transform.rotation.y, target.transform.rotation.z);
+        transform.rotation = newPos2;
+    }
+}
---
+++ Mobile Endless Game/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10f;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector3(0, rb.velocity.y, speed);
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0, 0);
+    }
+}
