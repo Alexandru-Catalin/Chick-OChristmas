c5de6018a6c5e13850fcdf08ce2f92edabc675ef
Corvus Corax <alexandrumarcu479@gmail.com>
2020-12-14 20:02:28 +0000

    Player

--- Mobile Endless Game/Assets/Scripts/CameraTarget.cs
+++ Mobile Endless Game/Assets/Scripts/CameraTarget.cs
@@ -9,11 +9,11 @@ public class CameraTarget : MonoBehaviour
     void Update()
     {
         //Camera will follow the Target at Z position - 10 and freezes positions X and Y
-        Vector3 newPos = new Vector3(0, target.transform.position.y + 3, target.transform.position.z - 10);
+        Vector3 newPos = new Vector3(0, target.transform.position.y + 6, target.transform.position.z - 8);
         transform.position = newPos;
 
         //Freez camera Rotations on X, Y, Z
-        Quaternion newPos2 = Quaternion.Euler(transform.rotation.x + 10, transform.rotation.y, target.transform.rotation.z);
+        Quaternion newPos2 = Quaternion.Euler(transform.rotation.x + 16, transform.rotation.y, target.transform.rotation.z);
         transform.rotation = newPos2;
     }
 }
