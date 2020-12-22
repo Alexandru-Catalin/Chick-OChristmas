3065cef2d34d66b86f0dff7cab79781d929133a2
Corvus Corax <alexandrumarcu479@gmail.com>
2020-12-22 13:56:12 +0000

    LoadingScreen

--- Mobile Endless Game/Assets/Scripts/MainMenu.cs
+++ Mobile Endless Game/Assets/Scripts/MainMenu.cs
@@ -2,14 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public GameObject loadingScreen;
+    public Slider slider;
+
     //Load scene
-    public void PlayGame()
+    public void PlayGame(int sceneIndex)
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene(1);
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+
+            yield return null;
+        }
     }
 
     public void QuitGame()
